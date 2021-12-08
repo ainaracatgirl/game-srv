a137b2c71776a28844e8bc60c93da4d464fdd668
JanCraft <jancraft888@gmail.com>
2021-12-08 09:41:11 +0100

    Added request handler

--- app.js
+++ app.js
@@ -5,15 +5,11 @@ const fs = require("fs");
 const server = HttpsServer({
     cert: fs.readFileSync(process.env.GAME_SRV_CERT),
     key: fs.readFileSync(process.env.GAME_SRV_KEY)
-})
+}, (req, res) => {
+    res.writeHead(200);
+    res.write(req.url);
+});
 const wss = new WebSocket({ server });
-
-server.on('connection', sock => {
-    console.log("conn");
-});
-server.on('error', err => {
-    console.error(err);
-});
 
 server.listen(443, () => {
     console.log(`Server listening on port 443`);
