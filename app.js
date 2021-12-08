116e77908a737b0a79e71ef5ce6eaca87b72bf9c
JanCraft <jancraft888@gmail.com>
2021-12-08 09:36:07 +0100

    testing

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const WebSocket = require("ws").Server;
 const HttpsServer = require('https').createServer;
 const fs = require("fs");
 
-server = HttpsServer({
+const server = HttpsServer({
     cert: fs.readFileSync(process.env.GAME_SRV_CERT),
     key: fs.readFileSync(process.env.GAME_SRV_KEY)
 })
-socket = new WebSocket({
-    server
-});
+const wss = new WebSocket({ server });
+
+server.on('connection', (sock) => {
+    console.log("conn");
+})
 
 server.listen(443);
